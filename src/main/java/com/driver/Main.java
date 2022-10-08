d52a7001f1148a212fed459df6b9b555dee4872b
Puspendu Saha <puspendusaha154@gmail.com>
2022-10-08 11:44:23 +0530

    Overridden is Added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,20 +3,19 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
 
-        B obj=new B();
-        String ans=obj.meth();
-        System.out.println(ans);
+        B b=new B();
+        b.meth();
     }
-}
 
-class A{
-    String meth(){
-        return "Invoking method from class A";
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
     }
-}
 
-class B extends A{
-    String meth(){
-        return "Method is overridden in Extendend class B";
+    public static class B extends A {
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
     }
 }
